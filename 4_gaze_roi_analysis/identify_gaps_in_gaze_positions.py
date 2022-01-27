e41c4bdc8c4b7ca8e8a58cf14c23a76b6adc112f
Joris W. van Rijn <hi@joriswvanrijn.com>
2022-01-27 14:21:38 +0100

    rolling average on confidence

--- 4_gaze_roi_analysis/identify_gaps_in_gaze_positions.py
+++ 4_gaze_roi_analysis/identify_gaps_in_gaze_positions.py
@@ -27,9 +27,12 @@ def identify_gaps_in_gaze_positions(participant_id, video_id, progress, task):
     total_count = df.shape[0]
 
     # Rolling average on confidence
-    df['confidence'] = df['confidence'].rolling(3, center=True).mean()
-    df.iloc[0, df.columns.get_loc('confidence')] = df.iloc[0, df.columns.get_loc('confidence')]
-    df.iloc[-1, df.columns.get_loc('confidence')] = df.iloc[-1, df.columns.get_loc('confidence')]
+    df['confidence_SRM'] = df['confidence'].rolling(3, center=True).mean()
+    df.iloc[0, df.columns.get_loc('confidence_SRM')] = df.iloc[0, df.columns.get_loc('confidence')]
+    df.iloc[-1, df.columns.get_loc('confidence_SRM')] = df.iloc[-1, df.columns.get_loc('confidence')]
+
+    print(df.head())
+    sys.exit()
 
     # Save the size of the original data set
     with open(text_file,"w+") as f:
